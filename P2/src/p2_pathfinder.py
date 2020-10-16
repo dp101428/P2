66f36e73505898824d990e322e7d7c75fd29814c
jboy5544123 <jocobrow@ucsc.edu>
2020-10-16 15:53:32 -0700

    commented sections

--- P2/src/p2_pathfinder.py
+++ P2/src/p2_pathfinder.py
@@ -1,4 +1,30 @@
-def find_path (source_point, destination_point, mesh):
-    """    Searches for a path from source_point to destination_point through the mesh    Args:        source_point: starting point of the pathfinder        destination_point: the ultimate goal the pathfinder must reach        mesh: pathway constraints the path adheres to
-    Returns:
-        A path (list of points) from source_point to destination_point if exists        A list of boxes explored by the algorithm    """    path = []    boxes = {}    return path, boxes.keys()
+
+def find_path (source_point, destination_point, mesh):
+
+    """
+    Searches for a path from source_point to destination_point through the mesh
+
+    Args:
+        source_point: starting point of the pathfinder
+        destination_point: the ultimate goal the pathfinder must reach
+        mesh: pathway constraints the path adheres to
+
+    Returns:
+
+        A path (list of points) from source_point to destination_point if exists
+        A list of boxes explored by the algorithm
+    """
+
+#identify the source and Destination boxes
+
+#Modify your Dijkstra's search to compute a legal list of line segments demonstrating the path
+
+#Convert Dijkstra's into A*
+
+#Modify your A* into a Bidirectional A*
+
+
+    path = []
+    boxes = {}
+
+    return path, boxes.keys()
