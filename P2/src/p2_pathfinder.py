ada8a193a39dd0175a50612cc02db325b71a85cd
jboy5544123 <jocobrow@ucsc.edu>
2020-10-18 22:31:59 -0700

    more bugfixing

--- P2/src/p2_pathfinder.py
+++ P2/src/p2_pathfinder.py
@@ -118,6 +118,7 @@ def find_path (source_point, destination_point, mesh):
                         path.append(boxes[priorNode])
                         priorNode = cameFromBack[priorNode]
                     #path.append(destination_point)
+                    path.insert(0, boxes[box])
                     priorNode = cameFromFront[box]
                     while priorNode is not None:
                         path.insert(0,boxes[priorNode])
