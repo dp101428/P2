3d33431a3be1419bb955e9cfc5aa04b75ef03616
Darcy Phipps <diphipps@ucsc.edu>
2020-10-19 16:37:26 -0700

    Improved path within cells and handled goal=start

--- P2/src/p2_pathfinder.py
+++ P2/src/p2_pathfinder.py
@@ -40,9 +40,9 @@ def find_path (source_point, destination_point, mesh):
         print("No Path")
         return path, boxes.keys()
 
-	#scan through boxes to find the destination point
-
-	#return a list of the 2 boxes with the source and destination
+    #If both the destination and the source are in the same box, shortcut everything
+    if(startingBox == goalBox):
+        return [source_point, destination_point], {startingBox : source_point}
 
 
 
@@ -87,15 +87,14 @@ def find_path (source_point, destination_point, mesh):
         
         #Otherwise, keep finding things to put in the queue
         for box in mesh["adj"][nextNode]:
-            pathToNew = shortest_path_to_box(boxes[nextNode], nextNode, box)
+            pathToNew = shortest_path_to_box(boxes[nextNode], nextNode, box, source_point if  goal == "start" else destination_point)
             lengthOfPath = pythagDist(pathToNew[0], pathToNew[1], boxes[nextNode][0], boxes[nextNode][0])
             if (goal == "dest"):
                 if(box in costToBack):
                     path.append(pathToNew)
-                    print(goal)
-                    print(boxes[box])
-                    print(boxes[nextNode])
-                    #Do this later
+                    #print(goal)
+                    #print(boxes[box])
+                    #print(boxes[nextNode])
                     path.append(boxes[box])
                     priorNode = cameFromBack[box]
                     while priorNode is not None:
@@ -119,10 +118,9 @@ def find_path (source_point, destination_point, mesh):
             else:
                 if(box in costToFront):
                     path.append(pathToNew)
-                    print(goal)
-                    print(boxes[box])
-                    print(boxes[nextNode])
-                    #Do this later
+                    #print(goal)
+                    #print(boxes[box])
+                    #print(boxes[nextNode])
                     path.append(boxes[nextNode])
                     priorNode = cameFromBack[nextNode]
                     while priorNode is not None:
@@ -148,12 +146,6 @@ def find_path (source_point, destination_point, mesh):
 
     #Debug since currently it doesn't draw
     print(path)
-#Convert Dijkstra's into A*
-
-#Modify your A* into a Bidirectional A*
-
-
-#use the shortest_path_to_box function to find the precise path between boxes
 
 
 
@@ -175,7 +167,7 @@ def resolvePathfinding(path, boxes):
     print (path)
     return path, boxes.keys()
 
-def shortest_path_to_box(current_point, current_box, new_box):
+def shortest_path_to_box(current_point, current_box, new_box, goalPoint):
     """
     new_x1 = new_box[0]
     new_x2 = new_box[1]
@@ -216,7 +208,7 @@ def shortest_path_to_box(current_point, current_box, new_box):
     """
     xBorder = (max(current_box[0], new_box[0]), min(current_box[1], new_box[1]))
     yBorder = (max(current_box[2], new_box[2]), min(current_box[3], new_box[3]))
-
+    """
     if current_point[0] <= xBorder[0] :
     	returnx = xBorder[0]
     elif current_point[0] <= xBorder[1] :
@@ -230,7 +222,42 @@ def shortest_path_to_box(current_point, current_box, new_box):
     	returny = current_point[1]
     else :
     	returny = yBorder[1]
+    """
+    returnx = -1
+    returny = -1
+    #If the border is parallel with the x axix, constrain x
+    if(xBorder[0] == xBorder[1]):
+        returnx = xBorder[0]
+    #If the border is parallel with the y axix, constrain y
+    if(yBorder[0] == yBorder[1]):
+        returny = yBorder[0]
+        #If x isn't constrained, evaluate it
+        if(returnx != xBorder[0]):
+            topEst = pythagDist(current_point[0], current_point[1], xBorder[0], returny) + pythagDist(xBorder[0], returny, goalPoint[0], goalPoint[1])
+            bottomEst = pythagDist(current_point[0], current_point[1], xBorder[1], returny) + pythagDist(xBorder[1], returny, goalPoint[0], goalPoint[1])
 
+            if topEst < bottomEst:
+                returnx = xBorder[0]
+            else:
+                returnx = xBorder[1]
+            if(returnx != current_point[0] and current_point[0] > xBorder[0] and current_point[0] < xBorder[1]):
+                middleEst = pythagDist(current_point[0], current_point[1], current_point[0], returny) + pythagDist(current_point[0], returny, goalPoint[0], goalPoint[1])
+                if(middleEst < bottomEst and middleEst < topEst):
+                    returnx = current_point[0]
+    #If y isn't constrained, evaluate it
+    if(returny != yBorder[0]):
+        topEst = pythagDist(current_point[0], current_point[1], returnx, yBorder[0]) + pythagDist(returnx, yBorder[0], goalPoint[0], goalPoint[1])
+        bottomEst = pythagDist(current_point[0], current_point[1], returnx, yBorder[1]) + pythagDist(returnx, yBorder[1], goalPoint[0], goalPoint[1])
+
+        if topEst < bottomEst:
+            returny = yBorder[0]
+        else:
+            returny = yBorder[1]
+        if(returny != current_point[1] and current_point[1] > yBorder[0] and current_point[1] < yBorder[1]):
+            middleEst = pythagDist(current_point[0], current_point[1], returnx, current_point[1]) + pythagDist(returnx, current_point[1], goalPoint[0], goalPoint[1])
+            if(middleEst < bottomEst and middleEst < topEst):
+                returny = current_point[1]
+    #Theoretically the closest point to the destination could be none of these (directly in line with the destination) but that will probably be rare and this is long enough
     return (returnx, returny)
 
 
